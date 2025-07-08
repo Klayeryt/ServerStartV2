1fe688c885ccd40151ada1172c6ec29319112c1e
Xenomoros <90210691+Klayeryt@users.noreply.github.com>
2025-07-08 18:29:10 +0700

    Full project c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ServerStartv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать файл";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(452, 262);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(389, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Запустить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(308, 325);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Выключить";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 360);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ServerStartV2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ServerStartv2
+{
+    public partial class Form1 : Form
+    {
+        Process process;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Выбранный файл: Отcутствует";
+            label1.BackColor = Color.Red;
+            label1.ForeColor = Color.White;
+            label2.Text = "Статус: Не выбран файл";
+            label2.BackColor = Color.Gray;
+            label2.ForeColor = Color.White;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "JavaScript files (*.js)|*.js|All files (*.*)|*.*";
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.DefaultExt = "js";
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = "Выбранный файл: " + openFileDialog1.FileName;
+                label1.BackColor = Color.White;
+                label1.ForeColor = Color.Green;
+                label2.Text = "Статус: Оффлайн";
+                label2.BackColor = Color.DarkRed;
+                label2.ForeColor = Color.White;
+            }
+        }
+
+        // Потокобезопасный вывод в RichTextBox
+        private void AppendToRichTextBox(RichTextBox box, string text, Color? color = null)
+        {
+            if(box.InvokeRequired)
+            {
+                box.Invoke((MethodInvoker)delegate
+                { AppendToRichTextBox(box, text, color); });
+                return;
+            }
+
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            if(color.HasValue)
+            {
+                box.SelectionColor = color.Value;
+            }
+
+            box.AppendText(text);
+            box.ForeColor = Color.White;
+            box.ScrollToCaret();
+        }
+
+        private async Task RunConsoleOutput(RichTextBox box, ProcessStartInfo startInfo) 
+        {
+            try
+            {
+                using (process)
+                {
+                    process.StartInfo = startInfo;
+                    process.OutputDataReceived += (send, data) =>
+                    {
+                        if (!string.IsNullOrEmpty(data.Data)) AppendToRichTextBox(richTextBox1, data.Data, Color.Green);
+                    };
+                    process.ErrorDataReceived += (send, err) =>
+                    {
+                        if (!string.IsNullOrEmpty(err.Data)) AppendToRichTextBox(richTextBox1, err.Data, Color.DarkRed);
+                    };
+
+                    process.Start();
+                    label2.Text = "Статус: Онлайн";
+                    label2.ForeColor = Color.Green;
+                    label2.BackColor = Color.White;
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
+
+                    await Task.Run(() => process.WaitForExit());
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendToRichTextBox(richTextBox1, $"Ошибка: {ex.Message}\n", Color.DarkRed);
+                label2.Text = "Статус: Оффлайн";
+                label2.ForeColor = Color.White;
+                label2.BackColor = Color.DarkRed;
+            }
+        }
+        
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var ProcessInfo = new ProcessStartInfo
+            {
+                FileName = @"C:\Program Files\nodejs\node.exe",
+                Arguments = $"\"{openFileDialog1.FileName}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                StandardOutputEncoding = System.Text.Encoding.UTF8,
+                StandardErrorEncoding = System.Text.Encoding.UTF8
+            };
+            if (openFileDialog1.FileName != null)
+            {
+                process = new Process();
+                await RunConsoleOutput(richTextBox1, ProcessInfo);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            try
+            {
+                if (process.HasExited != false)
+                {
+                    process.Close();
+                    label2.Text = "Статус: Оффлайн";
+                    label2.ForeColor = Color.White;
+                    label2.BackColor = Color.DarkRed;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Этот процесс уже завершён!");
+                label2.Text = "Статус: Оффлайн";
+                label2.ForeColor = Color.White;
+                label2.BackColor = Color.DarkRed;
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
